79043e575211af815e430e50ba180eed8245211a
Andre Rocha <andrerochaspan@gmail.com>
2017-05-29 04:41:04 -0400

    finished P26 FINALLY

---
+++ domino/auxiliares.py
@@ -0,0 +1,152 @@
+from functools import reduce
+from abstratas import *
+
+# Notações:
+
+# m: mão
+# msa: mesa
+# mx: mão OU mesa
+# pda: pedra
+# pta: ponta
+# px: pedra OU ponta
+# n: número (índice) de ponta de mesa
+
+# Retorna soma dos lados de pta (int)
+
+def soma_pta(pta):
+
+    if len(pta) == 1:
+
+        return pta[0]
+
+    else:
+
+        return soma(pta[0], pta[1])
+
+# Retorna soma das pontas de msa (int)
+
+def soma_msa(msa):
+
+    return reduce(soma, [soma_pta(pta) for pta in msa])
+
+# Verifica se pda contém pta, complementa a funções pdas_para_pta e pda_joga_em_pta (boolean)
+
+def pda_contem_pta(pta, pda):
+
+    return pda[0] == pta or pda[1] == pta
+
+# Retorna lista de pedras em m que contém pta, complementa as funções dos problemas P10, P11 e P16 (list)
+
+def pdas_para_pta(pta, m):
+
+    return [pda for pda in m if pda_contem_pta(pta, pda)]
+
+# Verifica se é válido jogar pda em pta, complementa as funções ptas_para_pda e joga_pedra (boolean)
+
+def pda_joga_em_pta(pda, pta):
+
+    return pda != tuple(pta) and pda_contem_pta(pta[0], pda)
+
+# Retorna lista de números de ponta de msa que formam uma jogada válida com pda, complementa a função do problema P20 e a função msas_possiveis (list)
+
+def ptas_para_pda(pda, msa):
+
+    return [n for n, pta in enumerate(msa) if pda_joga_em_pta(pda, pta)]
+
+# Retorna quantidade de elementos iguais a px em mx, complementa a função contem_duplicidades (int)
+
+def qnt_iguais(px, mx):
+
+    from domino import pedra_igual_p
+
+    if type(mx) is list:
+
+        return len([pda for pda in mx if pedra_igual_p(pda, px)])
+
+    else:
+
+        return len([pta for pta in mx if pta == px])
+
+# Verifica se mx contém elementos iguais, complementa as funções maop e mesap (boolean)
+
+def contem_duplicidades(mx):
+
+    if type(mx) is list:
+
+        return len([qnt_iguais(pda, mx) - 1 for pda in mx if qnt_iguais(pda, mx) - 1]) > 0
+
+    else:
+
+        return len([qnt_iguais(pta, mx) - 1 for pta in mx if qnt_iguais(pta, mx) - 1 and len(pta) == 2]) > 0
+
+# Verifica se pta é uma entrada válida para ponta de mesa (boolean)
+
+def pontap(pta):
+
+    from domino import pedrap, carrocap
+
+    if len(pta) != 2:
+
+        return 0 <= pta[0] <= 6
+
+    else:
+
+        return pedrap(pta) and carrocap(pta)
+
+# Verifica se mx contém elementos inválidos, complementa as funções maop e mesap (boolean)
+
+def contem_invalidos(mx):
+
+    from domino import pedrap
+
+    if type(mx) is list:
+
+        return len([pda for pda in mx if not pedrap(pda)]) > 0
+
+    else:
+
+        return len([pta for pta in mx if not pontap(pta)]) > 0
+
+# Retorna lista de duplas com a enésima ponta de msa e a combinação de pda em msa na ponta n, respectivamente, complementa a função pontos_pda_em_msa (list)
+
+def combinacoes_possiveis(pda, msa):
+
+    from domino import joga_pedra, mesap
+
+    return [(n, joga_pedra(pda, msa, n)) for n in ptas_para_pda(pda, msa) if mesap(joga_pedra(pda, msa, n))]
+
+# Retorna lista de duplas com a enésima ponta de msa e a pontuação da combinação de pda em msa na ponta n, respectivamente, complementa as funções dos problema P21 e P22 (list)
+
+def pontuacoes_de_pda_em_pontas(pda, msa):
+
+    from domino import pontos_marcados
+
+    return [(combinacao_possivel[0], pontos_marcados(combinacao_possivel[1])) for combinacao_possivel in combinacoes_possiveis(pda, msa)]
+
+# Retorna lista de duplas com a enésima pedra em m e sua lista de números de ponta que formam uma jogada válida em msa, respectivamente, complementa a função do problema P22 (list)
+
+def ns_para_m(m, msa):
+
+    return [(pda, ptas_para_pda(pda, msa)) for pda in m if ptas_para_pda(pda, msa) != []]
+
+# Retorna o número (índice) de pda em m, complementa a função gerar_indices (int)
+
+def n_de_pda_em_m(pda, m):
+
+    return [n for n, pdax in enumerate(m) if pdax == pda][0]
+
+# Retorna lista de índices com as maiores pontuações das pedras de m em msa (list)
+
+def indices_maiores_pontuacoes(m, msa):
+
+    from domino import maior_ponto
+
+    return [(n_de_pda_em_m(pda, m), maior_ponto(pda, msa)) for pda in m if maior_ponto(pda, msa) != None]
+
+# Retorna tupla com nova mesa gerada a partir dos índices e demais valores informados (tuple)
+
+def mesa_por_indice(indice, m, msa):
+
+    from domino import joga_pedra
+
+    return joga_pedra(m[indice[0]], msa, indice[1])
---
+++ domino/domino.py
@@ -0,0 +1,193 @@
+from auxiliares import *
+
+# Parte I – Bloco I
+
+# P01:
+
+def pedrap(pda):
+
+    return 0 <= pda[0] <= 6 and 0 <= pda[1] <= 6
+
+# P02:
+
+def maop(m):
+
+    return not contem_invalidos(m) and not contem_duplicidades(m) and len(m) <= 7
+
+# P03:
+
+def carrocap(pda):
+
+    return pda[0] == pda[1]
+
+# P04:
+
+def tem_carroca_p(m):
+
+    return len(tem_carrocas(m)) > 0
+
+# P05:
+
+def tem_carrocas(m):
+
+    return [pda for pda in m if carrocap(pda)]
+
+# Parte I – Bloco 2
+
+# P06:
+
+def pontos(m):
+
+    return reduce(soma, [soma(p[0], p[1]) for p in m])
+
+# P07
+
+def garagem(m):
+
+    if pontos(m) % 5 == 0:
+
+        return pontos(m)
+
+    else:
+
+        return [pontos(m) - n for n in range(1, 5) if (pontos(m) - n) % 5 == 0][0]
+
+# P08
+
+def pedra_igual_p(pda1, pda2):
+
+    return pda1 == pda2 or pda1[0] == pda2[1] and pda1[1] == pda2[0]
+
+# P09
+
+def ocorre_pedra_p(pda, m):
+
+    return len([pdax for pdax in m if pedra_igual_p(pdax, pda)]) > 0
+
+# P10
+
+def ocorre_valor_p(pta, m):
+
+    return len(pdas_para_pta(pta, m)) > 0
+
+# P11
+
+def ocorre_pedra(pta, m):
+
+    return pdas_para_pta(pta, m)
+
+# P12
+
+def pedra_maior(m):
+
+    return maior(m)
+
+# P13
+
+def ocorre_valor_q(pta, m):
+
+    return len(ocorre_pedra(pta, m))
+
+# P14
+
+def ocorre_carroca_q(m):
+
+    return len(tem_carrocas(m))
+
+# P15
+
+def tira_maior(m):
+
+    return [pda for pda in m if not pedra_igual_p(pda, pedra_maior(m))]
+
+# P16
+
+def tira_maior_v(pta, m):
+
+    return pedra_maior(pdas_para_pta(pta, m))
+
+# Parte I – Bloco 3
+
+# P17
+
+def mesap(msa):
+
+    return not contem_invalidos(msa) and not contem_duplicidades(msa) and len(msa) == 4
+
+# P18
+
+def carroca_m_p(msa):
+
+    return len([pta for pta in msa if len(pta) == 2 and carrocap(pta)]) > 0
+
+# P19
+
+def pontos_marcados(msa):
+
+    if multiplo_de_cinco(soma_msa(msa)):
+
+        return soma_msa(msa)
+
+    else:
+
+        return 0
+
+# P20
+
+def pode_jogar_p(pda, msa):
+
+    return len(ptas_para_pda(pda, msa)) > 0
+
+# P21
+
+def marca_ponto_p(pda, msa):
+
+    return len([pontuacao_de_pda_em_pontas[1] for pontuacao_de_pda_em_pontas in pontuacoes_de_pda_em_pontas(pda, msa) if pontuacao_de_pda_em_pontas[1] != 0]) > 0
+
+# P22
+
+def maior_ponto(pda, msa):
+
+    pontuacoes = [pontuacao_de_pda_em_pontas[1] for pontuacao_de_pda_em_pontas in pontuacoes_de_pda_em_pontas(pda, msa)]
+
+    if pontuacoes != []:
+
+        maior_pontuacao = maior(pontuacoes)
+    
+        return [pontuacao_de_pda_em_pontas[0] for pontuacao_de_pda_em_pontas in pontuacoes_de_pda_em_pontas(pda, msa) if pontuacao_de_pda_em_pontas[1] == maior_pontuacao][0]
+
+    # else:
+
+    #     return False
+
+# P23
+
+def joga_pedra(pda, msa, n):
+
+    msa = list(msa)
+
+    msa[n] = [pta for pta in pda if pta != msa[n][0] or carrocap(pda)]
+
+    return tuple(msa)
+
+# P24
+
+def jogap(m, msa):
+
+    return len(ns_para_m(m, msa)) > 0
+
+# P25
+
+def jogada(m, msa):
+
+    indices = indices_maiores_pontuacoes(m, msa)
+
+    maior_pontuacao = maior([soma_msa(mesa_por_indice(indice, m, msa)) for indice in indices])
+
+    return [indice for indice in indices if soma_msa(mesa_por_indice(indice, m, msa)) == maior_pontuacao]
+
+# P26
+
+def faz_jogada(m, msa):
+
+    return [mesa_por_indice(indice, m, msa) for indice in jogada(m, msa)]
