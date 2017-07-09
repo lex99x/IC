b3eeb438740a367b4fc0367e97cbbfeae7833528
Alex A <andrerochaspan@gmail.com>
2017-07-08 20:53:20 -0400

    improved norep function

--- exercicios/norep.py
+++ exercicios/norep.py
@@ -2,17 +2,13 @@
 
 # Escreva uma definição recursiva para a função norep(xs), cuja avaliação associe uma lista similar a xs porém sem nenhum elemento repetido.
 
-def ocorre(k, xs):
-
-    return len([x for x in xs if x == k]) > 0
-
 def norep(xs):
 
     if len(xs) == 0:
 
         return []
 
-    elif not ocorre(xs[0], xs[1:]):
+    elif xs[0] not in xs[1:]:
 
         return [xs[0]] + norep(xs[1:])
 
